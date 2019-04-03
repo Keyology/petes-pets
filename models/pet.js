0ea77bf8940b34c2ebc6d50f58ad2316c620dd2b
Keyology <keonimurray45@gmail.com>
2019-04-02 18:12:46 -0700

    added pagination to the home page

--- models/pet.js
+++ models/pet.js
@@ -1,19 +1,28 @@
 "use strict";
 
-const mongoose = require('mongoose'),
-        Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate");
+const Schema = mongoose.Schema;
 
-const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
-},
-{
-  timestamps: true
-});
+mongoosePaginate.paginate.options = {
+  limit: 3 //how many records to show on each page
+};
 
-module.exports = mongoose.model('Pet', PetSchema);
+const PetSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    species: { type: String },
+    birthday: { type: Date },
+    picUrl: { type: String },
+    picUrlSq: { type: String },
+    favoriteFood: { type: String },
+    description: { type: String }
+  },
+  {
+    timestamps: true
+  }
+);
+
+PetSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Pet", PetSchema);
